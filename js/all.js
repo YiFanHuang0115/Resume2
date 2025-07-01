66f7009d64be4f81de85f363c7615d120fdb794b
YiFanHuang0115 <95221343+YiFanHuang0115@users.noreply.github.com>
2025-07-01 13:15:41 +0800

    ver.001

---
+++ js/all.js
@@ -0,0 +1,45 @@
+// $(document).ready(function(){
+//     $('.timeline p, .timeline2 p').hide();
+//     $(".timeline li:nth-child(1)").click(function(event){
+//         $('#dialog_1').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(2)").click(function(event){
+//         $('#dialog_2').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(3)").click(function(event){
+//         $('#dialog_3').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(4)").click(function(event){
+//         $('#dialog_4').dialog({width: 640});
+//     });
+//     $(".timeline2 li:nth-child(1)").click(function(event){
+//         $('#dialog_5').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(2)").click(function(event){
+//         $('#dialog_6').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(3)").click(function(event){
+//         $('#dialog_7').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(4)").click(function(event){
+//         $('#dialog_8').dialog({width: 375});
+//     });
+
+// })
+
+
+
+const arrswitch = document.querySelector('#switch');
+let classList = arrswitch.classList;
+let autobiography = document.querySelector(".autobiography p");
+arrswitch.addEventListener('click', function(){
+    const result = classList.toggle(0);
+    if(result){
+        autobiography.setAttribute("class", "d-none fs-5");
+        arrswitch.innerHTML = `<i class="fs-1 fas fa-chevron-circle-up"></i>`;
+    } else {
+        autobiography.setAttribute("class", "d-block fs-5");
+        arrswitch.innerHTML = `<i class="fs-1 fas fa-chevron-circle-down"></i>`;
+    }
+})
+
